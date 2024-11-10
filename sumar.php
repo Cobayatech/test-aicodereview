f363cbafa897e40849be80b32b716bc9d2983cd4
alvaro-barrera <abarrera@buk.pe>
2024-11-10 12:45:12 -0500

    first commit

---
+++ sumar.php
@@ -0,0 +1,18 @@
+<?php
+
+// Definir una función que suma dos números
+function sumar($numero1, $numero2) {
+    return $numero1 + $numero2;
+}
+
+// Ejemplo de uso
+$numero1 = 10;
+$numero2 = 20;
+
+// Llamar a la función y almacenar el resultado
+$resultado = sumar($numero1, $numero2);
+
+// Mostrar el resultado
+echo "La suma de $numero1 y $numero2 es: $resultado";
+
+?>
